72a385f368a6687b9f77b93d3fb43cdfbe088e63
Pascal Schärli <pascscha@student.ethz.ch>
2018-12-04 18:37:55 +0100

    Added examples

--- deven.py
+++ deven.py
@@ -1,53 +1,85 @@
 import sys
 
-def push(c:chr):
+def push(i:int):
 	global memoryStack
-	memoryStack.append(c)
+	memoryStack.append(i)
 
 def inputChar():
 	global memoryStack
 	r = input()
-	memoryStack.append(r[0])
+	memoryStack.append(ord(r[0]))
 
 def outputChar():
 	global memoryStack
-	print(memoryStack[-1],end="")
+	print(chr(memoryStack[-1]),end="")
 	memoryStack = memoryStack[:-1]
 
 def pop():
 	global memoryStack
-	r =memoryStack[-1]
+	r = memoryStack[-1]
 	memoryStack = memoryStack[0:-1]
 	return r
 
 def add():
 	global memoryStack
+	l = memoryStack[-2]
+	r = memoryStack[-1]
+	memoryStack = memoryStack[:-2]
+	memoryStack.append(l+r)
+
+def sub():
+	global memoryStack
+	l = memoryStack[-2]
+	r = memoryStack[-1]
+	memoryStack = memoryStack[:-2]
+	memoryStack.append(l-r)
+
+def jump():
+	global iptr, memoryStack
+	if memoryStack[-2] == 1:
+		iptr += memoryStack[-1]
+		#print("jump to ",iptr,"(+",memoryStack[-1],")")
+	memoryStack = memoryStack[:-2]
+
+def duplicate(i:int):
+	global memoryStack
+	memoryStack.append(memoryStack[-(i+1)])
+
+def delete(i:int):
+	global memoryStack
+	memoryStack.pop(len(memoryStack)-(i+1))
+
+def equal():
+	global memoryStack
 	l = ord(memoryStack[-2])
 	r = ord(memoryStack[-1])
 	memoryStack = memoryStack[:-2]
-	memoryStack.append(chr((l+r)%128))
+	memoryStack.append(int(l==r))
 
-def jump(i:int):
-	global iptr, memoryStack
-	if memoryStack[-1] == "D":
-		memoryStack = memoryStack[:-1]
-		iptr = i
-	
+def smaller():
+	global memoryStack
+	l = memoryStack[-2]
+	r = memoryStack[-1]
+	memoryStack = memoryStack[:-2]
+	memoryStack.append(int(l<r))
+
 def parseInt(program, iptr):
 	n = ""
-	while iptr < len(program) and program[iptr] in "0123456789":
+	while iptr < len(program) and program[iptr] in "-0123456789":
 			n = n+program[iptr]
 			iptr+=1
 	if len(n) == 0:
-		return 0
-	return iptr,int(n)
+		return 0, 0
+	return iptr, int(n)
+
 
 def parseChar(program, iptr):
 	return iptr+1,program[iptr]
 
 def step(program):
-	global iptr
+	global iptr 
 	instruction = instructions[program[iptr]]
+	#print(program[iptr])
 	if instruction[0] == int:
 		iptr += 1
 		iptr, arg = parseInt(program, iptr)
@@ -60,22 +92,34 @@ def step(program):
 		iptr +=1
 		instruction[1]()
 
-instructions = {	"p":[chr,push],
+instructions = {	"p":[int,push],
 					"i":[None,inputChar],
 					"o":[None,outputChar],
 					"P":[None,pop],
 					"a":[None,add],
-					"j":[int,jump]}
+					"s":[None,sub],
+					"j":[None,jump],
+					"d":[int,duplicate],
+					"D":[int,delete],
+					"e":[None,equal],
+					"S":[None,smaller]
+				}
 
 
 if __name__ == "__main__":
+	memoryStack = [0] #default return address
+	iptr = 0
+
 	with open(sys.argv[1]) as f:
 		program = f.read()
+	if len(sys.argv) > 2:
+		for i in range(2,len(sys.argv)):
+			memoryStack.append(int(sys.argv[i]))
 
-	memoryStack = []
-	iptr = 0
 
 	while iptr < len(program):
 		step(program)
+		#print("{}: {}".format(iptr, memoryStack))
+		#input()
+	print("\nFinished: ptr = {}, memoryStack = {}".format(iptr, memoryStack))
 
-	print("\nMemory after execution: "+str(memoryStack))
---
+++ utils/string_to_deven.py
@@ -0,0 +1,4 @@
+string = input("Enter your string: ")
+for s in string:
+	print("p{}o".format(ord(s)),end="")
+print()
